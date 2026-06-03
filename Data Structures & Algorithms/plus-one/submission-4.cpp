342a03869b19278e547cbd62390ebbf94258b431
蔡侑傑 <134054572+Edison1019@users.noreply.github.com>
2026-06-03 18:26:18 +0800

    Add: plus-one - submission-4

---
+++ Data Structures & Algorithms/plus-one/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        bool carry = true;
+        int i = 0;
+        reverse(digits.begin(), digits.end());
+        while(carry){
+            if(i < digits.size()){
+                if(digits[i] == 9){
+                    digits[i] = 0;
+                }
+                else{
+                    digits[i] += 1;
+                    carry = false;
+                }
+            }
+            else{
+                digits.push_back(1);
+                carry = false;
+            }
+            i++;
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
