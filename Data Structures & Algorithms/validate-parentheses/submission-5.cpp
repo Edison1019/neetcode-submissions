96049bfe163c9f58ae85b55abdde6ad7a6c593c7
蔡侑傑 <134054572+Edison1019@users.noreply.github.com>
2026-06-07 18:43:46 +0800

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> u ={
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for(char c : s){
+            if(!u.count(c)){
+                stack.push(c);
+            }
+            else{
+                if(!stack.empty() && u[c] == stack.top()){
+                    stack.pop();
+                }
+                else
+                    return false;
+            }
+        }
+
+        return stack.empty();
+    }
+};
