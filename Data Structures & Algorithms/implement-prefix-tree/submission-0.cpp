b5ebf8e371fafd1cd9ad5db704427d9522fa5bf5
蔡侑傑 <134054572+Edison1019@users.noreply.github.com>
2026-07-09 16:20:29 +0800

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,46 @@
+class TrieNode{
+public:
+    unordered_map<char, TrieNode*> children;
+    bool end = false;
+};
+
+class PrefixTree {
+public:
+    TrieNode* root;
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* cur = root;
+        for(char c : word){
+            if(cur->children[c] == nullptr) 
+                cur->children[c] = new TrieNode();
+            
+            cur = cur->children[c];
+        }
+        cur->end = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* cur = root;
+        for(char c : word){
+            if(cur->children[c] == nullptr)
+                return false;
+        
+            cur = cur->children[c];
+        }
+        return cur->end;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* cur = root;
+        for(char c : prefix){
+            if(cur->children[c] == nullptr)
+                return false;
+
+            cur = cur->children[c];   
+        }
+        return true;
+    }
+};
