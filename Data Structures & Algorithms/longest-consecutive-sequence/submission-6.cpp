edda393c9684d338b6d785ef35ca068140a38376
蔡侑傑 <134054572+Edison1019@users.noreply.github.com>
2026-08-05 15:41:58 +0800

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        if(nums.size() == 1) return 1;
+
+        set<int> s;
+        for(int num : nums) s.insert(num);
+        int start = *s.begin();
+        s.erase(s.begin());
+        int result = 1, cnt = 1;
+
+        while(!s.empty()){
+            int current = *s.begin();
+            if(current - start == 1){
+                cnt++;
+                result = max(result, cnt);
+            }
+            else{
+                cnt = 1;
+            }
+            start = current;
+            s.erase(s.begin());
+        }
+
+        return result;
+    }
+};
