1f54cfac4a4be6cd4845a9d8b682d10d46a12c76
蔡侑傑 <134054572+Edison1019@users.noreply.github.com>
2026-08-08 21:41:25 +0800

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res = 0;
+        unordered_set<char> set;
+        for(char c : s) set.insert(c);
+
+        for(char c : set){
+            int l = 0, cnt = 0;
+            for(int r=l; r<s.size(); r++){
+                if(s[r] == c) cnt++;
+
+                while((r-l+1) - cnt > k){
+                    if(s[l] == c) cnt--;
+                    l++;
+                }
+                res = max(res, r-l+1);
+            }
+
+        }
+        return res;
+        
+    }
+};
